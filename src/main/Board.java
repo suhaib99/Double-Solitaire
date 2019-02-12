4b940844de853ce53e8901c8617194c2408b15bd
Britain Van Bergeyk <brit.vb@hotmail.com>
2019-02-12 14:59:33 -0700

    Basic Layout of project Board, Card, Deck, and Main classes added

--- src/main/Board.java
+++ src/main/Board.java
@@ -8,4 +8,23 @@ public class Board {
     private ArrayList<Card> foundation3 = new ArrayList<>();
     private ArrayList<Card> foundation4 = new ArrayList<>();
 
+    private ArrayList<Card> pile1 = new ArrayList<>();
+    private ArrayList<Card> pile2 = new ArrayList<>();
+    private ArrayList<Card> pile3 = new ArrayList<>();
+    private ArrayList<Card> pile4 = new ArrayList<>();
+    private ArrayList<Card> pile5 = new ArrayList<>();
+    private ArrayList<Card> pile6 = new ArrayList<>();
+    private ArrayList<Card> pile7 = new ArrayList<>();
+
+    private ArrayList<Card> discardPile = new ArrayList<>();
+
+    Board(){
+        this.shuffle();
+
+    }
+
+    private void shuffle(){
+        Deck mainDeck = new Deck();
+    }
+
 }
