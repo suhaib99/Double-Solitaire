2e5e401c498cc33eba1c8ec62234dc295c334061
Britain Van Bergeyk <brit.vb@hotmail.com>
2019-02-13 18:58:33 -0700

    Added getDeck method

--- src/main/Deck.java
+++ src/main/Deck.java
@@ -1,15 +1,28 @@
 package main;
 
+import java.util.Arrays;
+
 public class Deck {
-    private Card[] deck = new Card[52];
+    private int[][] deck = new int[52][2];
 
     Deck(){
         int index = 0;
         for (int i = 1; i <= 13; i++){
             for(int j = 1; j <= 4; j++){
-                deck[index] = new Card(j, i);
+                Card tempCard = new Card(j, i);
+                deck[index] = tempCard.toArray();
                 index++;
             }
         }
+    }
+
+    int[][] getDeck(){
+        return deck;
+    }
+
+    public static void main(String[] args){
+        Deck testDeck = new Deck();
+        System.out.println(Arrays.toString(testDeck.getDeck()[10]));
+        System.out.println(testDeck.getDeck()[10][1]);
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,7 @@ package main;
  *
  */
 public class Main {
-    public void main(String[] args){
+    public static void main(String[] args){
 
     }
 
