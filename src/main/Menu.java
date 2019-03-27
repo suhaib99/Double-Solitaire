f48edce0d18cfcdcabc132b68fb23ed82f5a5526
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2019-03-27 10:30:18 -0600

    Updated Game Menu

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -3,11 +3,17 @@ package main;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import static javafx.scene.paint.Color.GREEN;
+import java.io.FileInputStream;
 
 public class Menu extends Application {
 
@@ -22,24 +28,32 @@ public class Menu extends Application {
     {
 
         Pane root = new Pane();
-        scene = new Scene(layout,700,700, GREEN);
+
 
         Button first;
         first = new Button("New Game");
         Button second = new Button("Load Game");
         Button third = new Button("Quit Game ");
 
-        first.setLayoutX(290);
+        Image image = new Image(new FileInputStream("src\\res\\Decks\\pngs\\MenuScreen.png"));
+        ImageView mv = new ImageView(image);
+
+        Text text = new Text();
+        text.setText("DOUBLE SOLITAIRE");
+        text.setX(385);
+        text.setY(50);
+        text.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR,40));
+        first.setLayoutX(575);
         first.setLayoutY(250);
-        second.setLayoutX(290);
+        second.setLayoutX(575);
         second.setLayoutY(300);
-        third.setLayoutX(290);
+        third.setLayoutX(575);
         third.setLayoutY(350);
         primaryStage.setTitle("Double_Solitaire");
-
-        root.getChildren().addAll(first,second,third);
-        primaryStage.setScene(new Scene(root,700,750));
-
+        root.getChildren().addAll(mv);
+        root.getChildren().addAll(first,second,third,text);
+        primaryStage.setScene(new Scene(root,1200,630));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
